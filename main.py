0f48ec315c6b042ad8d0afc7f220fe46fd6ace6f
ADS-140605 <anadidevsoni2005@gmail.com>
2026-05-10 22:33:26 +0530

    PUTin...g2

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, HTTPException, Path ,Query
 from fastapi.responses import JSONResponse
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 import json
 from pydantic import BaseModel
 from pydantic import Field,computed_field
+
+
+
 
 class Patient(BaseModel):
     id: Annotated[str, Field(..., description="The ID of the patient", example="P001")]
@@ -28,15 +31,29 @@ class Patient(BaseModel):
         else:
             return "Obese"
         
+
+
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(None, description="The name of the patient", example="John Doe")]
+    age: Annotated[Optional[int], Field(None,gt=0,lt=150, description="The age of the patient", example=30)]
+    height: Annotated[Optional[float], Field(None,gt=0, description="The height of the patient", example=175.5)]
+    weight: Annotated[Optional[float], Field(None,gt=0, description="The weight of the patient", example=70.0)]   
+    gender: Annotated[Optional[Literal["Male", "Female","Other"]], Field(None, description="The gender of the patient", example="Male")]       
+
+
 app = FastAPI()
 
 def load_data():
     with open("patients.json", "r") as f:
             data = json.load(f)
     return data
+
+
 def save_data(data):
     with open("patients.json", "w") as f:
         json.dump(data, f)
+
+
 
 @app.get("/")
 def base():
@@ -50,8 +67,6 @@ def about():
 def get_patients():
     data= load_data()
     return {"message": "List of patients", "patients": data}
-
-
 
 @app.get("/patients/{patient_id}")
 def get_patient(patient_id: str =Path(..., description="The ID of the patient to retrieve", example="P001")):
@@ -72,6 +87,9 @@ def sort_patients(sort_by: str = Query(..., description="The field to sort patie
     sorted_patients = sorted(data.items(), key=lambda x: x[1][sort_by], reverse=(order == "desc"))
     return {"message": f"Patients sorted by {sort_by} in {order} order", "patients": dict(sorted_patients)}
 
+
+
+
 @app.post("/new_patient")
 def add_patient(patient: Patient):
     data = load_data()
@@ -80,6 +98,9 @@ def add_patient(patient: Patient):
     data[patient.id] = patient.model_dump(exclude={'id'})
     save_data(data)
     return JSONResponse(status_code=201, content={"message": "Patient added successfully", "patient": patient.dict()})
+
+
+
 
 @app.delete("/patients/{patient_id}")
 def delete_patient(patient_id: str = Path(..., description="The ID of the patient to delete", example="P001")):
@@ -90,12 +111,21 @@ def delete_patient(patient_id: str = Path(..., description="The ID of the patien
     save_data(data)
     return {"message": "Patient deleted successfully"}
 
+
+
 @app.put("/patients/{patient_id}")
-def update_patient(patient_id: str = Path(..., description="The ID of the patient to
-    update", example="P001"), updated_patient: Patient = None):
-        data = load_data()
-        if patient_id not in data:
-            raise HTTPException(status_code=404, detail="Patient not found")
-        data[patient_id] = updated_patient.model_dump(exclude={'id'})
-        save_data(data)
-        return {"message": "Patient updated successfully", "patient": updated_patient.dict()}
+def update_patient(patient_update: PatientUpdate , patient_id: str = Path(..., description="The ID of the patient to update", example="P001")):
+    data = load_data()
+    patient_id = patient_update.id
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    existing_patient = data[patient_id]
+    updated_patient = patient_update.model_dump(exclude_unset=True)
+    
+    for key, value in updated_patient.items():
+        existing_patient[key] = value
+    existing_patient['id'] = patient_id
+    existing_patient=Patient(**existing_patient)
+    data[patient_id] = existing_patient.model_dump(exclude={'id'})
+    save_data(data)
+    return {"message": "Patient updated successfully", "patient": existing_patient.dict()}
