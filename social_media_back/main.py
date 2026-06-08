c3e625d1b863ebe4935d48cc3204d054e2b2d529
ADS-140605 <anadidevsoni2005@gmail.com>
2026-06-08 17:38:14 +0530

    Data amnipulation using pandas is done using api endpoints and crud operations

--- social_media_back/main.py
+++ social_media_back/main.py
@@ -2,15 +2,23 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
-
+import pandas as pd
 import json
 app = FastAPI()
 
 def load_data():
     with open("social_media_dummy.json","r",encoding="utf-8") as file:
         data = json.load(file)
-
     return data
+
+def add_col(data,col_name):
+    df=pd.DataFrame(data)
+    df = df.fillna(value=0)
+    df[col_name]=df.get(col_name)
+    df["id"] = [f"{i:03}" for i in range(len(df))]
+    data=df.to_dict(orient="records")
+    return data
+
 def dump_data(data):
     with open("social_media_dummy.json","w",encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
@@ -29,12 +37,13 @@ def root():
 @app.get("/posts")
 def get_posts():
     data=load_data()
+    data=add_id_col(data)
     return {"data":data}
 
 @app.post("/createpost")
 def create_posts(data : Post):
     original_data=load_data()
     original_data.append(data.model_dump())
+    
     dump_data(original_data)
     return {"DATA":data}
-
