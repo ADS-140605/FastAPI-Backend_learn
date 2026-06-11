c1d42c45cf5e5beb42ab729a139903bc0661885b
ADS-140605 <anadidevsoni2005@gmail.com>
2026-06-11 16:39:40 +0530

    Put

--- social_media_back/main.py
+++ social_media_back/main.py
@@ -29,7 +29,11 @@ def del_col(data,col_name):
     df = df.drop(columns=col_name)
     data=df.to_dict(orient="records")
     return data
-
+class Update(BaseModel):
+    title: Optional[str]=None
+    content: Optional[str]=None
+    published: Optional[bool]=None
+    rating : Optional[int]=None
 
 class Post(BaseModel):
     title: str
@@ -74,4 +78,21 @@ def delete_data(id):
             dump_data(data)
             raise HTTPException(status_code=status_codes.HTTP_204_NO_CONTENT, detail="Post deleted successfully")
 
+    return {"message": "Post not found"}
+
+@app.put("/posts/{id}")
+def update_data(id,update:Update):
+    data=load_data()
+    for post in data:
+        if post.get("id") == id:
+            if update.title is not None:
+                post["title"]=update.title
+            if update.content is not None:
+                post["content"]=update.content
+            if update.published is not None:
+                post["published"]=update.published
+            if update.rating is not None:
+                post["rating"]=update.rating
+            dump_data(data)
+            return {"message":"Post updated successfully"}
     return {"message": "Post not found"}
