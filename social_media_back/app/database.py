a4932b58e2816ed0b0a1687f943585b97a185a9f
ADS-140605 <anadidevsoni2005@gmail.com>
2026-06-12 17:52:04 +0530

    new table for posts made

--- social_media_back/app/database.py
+++ social_media_back/app/database.py
@@ -11,3 +11,17 @@ conn = psycopg.connect(
 cursor = conn.cursor()
 
 print("Database connected successfully")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS posts(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    published BOOLEAN DEFAULT TRUE,
+    rating INTEGER
+)
+""")
+
+conn.commit()
+
+print("Table created successfully")
