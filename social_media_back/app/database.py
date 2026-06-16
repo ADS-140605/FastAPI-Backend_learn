0fea049e5dc4fbbfca1aca4026f7aa2d091bf983
ADS-140605 <anadidevsoni2005@gmail.com>
2026-06-16 16:59:47 +0530

    ORM setup

---
+++ social_media_back/app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip_adress>:<port>/<database_name>"
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
